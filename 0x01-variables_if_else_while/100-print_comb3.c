235224fe0992547e3f9b6a707e0f58f4ca7e4f3d
jokings <joshuaashua3@3.com>
2022-09-11 19:17:45 -0700

    comb comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,25 @@
  */
 int main(void)
 {
-	int d;
+	int n1 = 48;
+	int n2;
+	int com = 44;
 
-	for (d = '0'; d <= '9'; d++)
+	while (n1 <= 57)
 	{
-		putchar(d);
-		if (d != '9')
+		n2 = n1 + 1;
+		while (n2 <= 57)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(n1);
+			putchar(n2);
+			if (n1 != 56 || n2 != 57)
+			{
+				putchar(com);
+				putchar(32);
+			}
+			n2 += 1;
 		}
+		n1 += 1;
 	}
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,25 +7,16 @@
  */
 int main(void)
 {
-	int n1 = 48;
-	int n2;
-	int com = 44;
-
-	while (n1 <= 57)
+	int d;
+	
+	for (d = '0'; d <= '9'; d++)
 	{
-		n2 = n1 + 1;
-	while (n2 <= 57)
-	{
-		putchar(n1);
-		putchar(n2);
-	if (n1 != 56 || n2 != 57)
-	{
-		putchar(com);
-		putchar(32);
-	}
-		n2 += 1;
-	}
-		n1 += 1;
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
