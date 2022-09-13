1494b9936bb26acaec6917bc4b9b9badda42a117
jokings <joshuaashua3@3.com>
2022-09-13 15:20:13 -0700

    Creating a new directory, a readme file, and two files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+u}
