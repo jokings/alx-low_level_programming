b778ac361c15c04764074a39e86c1478e5c2e630
jokings <joshuaashua3@3.com>
2022-09-14 15:04:50 -0700

    main.h edidted

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
 int add(int one, int two);
-void print_to_98(int n)
+void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
