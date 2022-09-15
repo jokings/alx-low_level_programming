40577521af3e0c7c9ae4c74467ed174cc0bc6997
jokings <joshuaashua3@3.com>
2022-09-15 13:33:42 -0700

    new files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
@@ -14,7 +15,7 @@ void print_most_numbers(void)
 		if ((i == 2) || (i == 4))
 			continue;
 		else
-			_putchar(i + '0');
+			putchar(i + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
